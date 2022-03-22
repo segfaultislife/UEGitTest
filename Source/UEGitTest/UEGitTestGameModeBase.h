6b89827d49adf61725dd76cce7f173e232cd99d3
segfaultislife <mail@n30.quest>
2022-03-22 17:39:06 +0100

    Initial commit

---
+++ Source/UEGitTest/UEGitTestGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UEGitTestGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UEGITTEST_API AUEGitTestGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
